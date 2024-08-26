e59954f415428362355a0438b7fcc65802fce265
ajiahamed <ajiahamed@live.com>
2024-08-26 11:58:47 +0530

    Added screen shots for Dev Program

--- case_study_DevProgram/nw_client_side.py
+++ case_study_DevProgram/nw_client_side.py
@@ -1,7 +1,7 @@
 import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('localhost', 65432))
+client_socket.connect(('192.168.29.129', 65432))
 
 client_socket.sendall(b"Hello from network socket")
 client_socket.close()
--- case_study_DevProgram/nw_srv_side.py
+++ case_study_DevProgram/nw_srv_side.py
@@ -1,14 +1,15 @@
 import socket
-server_socket = socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('localhost', 65432))
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('192.168.29.129', 65432))
 server_socket.listen(1)
 
-print("Server waiting for a connection...")
+print("Server waiting for a connection from remote client...")
 connection, client_address = server_socket.accept()
 
 try:
   data = connection.recv(1024)
   print(f"Received: {data.decode()}")
+
 finally:
   connection.close()
   server_socket.close()
